3de6835e0f373440c4fb601564f775ac1fc301ba
Laura-SM <74296613+Laura-SM@users.noreply.github.com>
2023-06-09 09:46:46 +0200

    Update NodeMenu class and tests

--- src/Menu/NodeMenu/NodeMenu.ts
+++ src/Menu/NodeMenu/NodeMenu.ts
@@ -1,52 +1,22 @@
-import { type OptionsCollection } from '../OptionsCollection/OptionsCollection';
+import type MenuItemsCollection from '../MenuItemsCollection/MenuItemsCollection';
 import { type Menu } from '../interfaces/Menu';
 
-// export interface Option {
-//   value: string;
-//   label: string;
-// }
+class NodeMenu implements Menu {
+  #nodeMenuItems: MenuItemsCollection;
 
-// export type ObjectOption = Record<string, () => void>;
-
-// const options: Collection = [
-//   // { value: '0', label: 'Invalid' },
-//   { value: '1', label: 'Play Now' },
-//   { value: '2', label: 'Exit' },
-// ];
-
-export class NodeMenu implements Menu {
-  #options: OptionsCollection<string> | undefined;
-
-  constructor() {
-    this.#options = undefined;
+  constructor(menuItems: MenuItemsCollection) {
+    this.#nodeMenuItems = menuItems;
   }
 
   showMenu(): void {
     console.log('Select one option:');
-    const menuOptions = this.#options?.getList();
-    menuOptions?.forEach((option, index) => {
-      console.log(`${index + 1}. ${option}`);
+
+    const menuItemsList = this.#nodeMenuItems.getList();
+
+    menuItemsList.forEach((option, index) => {
+      console.log(`${index + 1}. ${option.getName()}`);
     });
-
-    // Manager class
   }
-
-  // this.#readLineNode.question('Write the number of your choice: ', answer => {
-  //     const userInputValue = parseInt(answer, 10);
-  //     // 3
-  //     this.#userSelectedOption = options[userInputValue - 1];
-
-  //     //  this.#userSelectedOption = options[userInputValue - 1];
-  //   });
-
-  // handleExit(): void {
-  //   this.#readLineNode.log('See you soon!');
-  //   this.#readLineNode.close();
-  // }
-
-  // handleInvalidValue(): void {
-  //   console.log('handleInvalidValue');
-  //   this.#readLineNode.log('Invalid option, please re-enter an option');
-  //   this.showMenu();
-  // }
 }
+
+export default NodeMenu;
